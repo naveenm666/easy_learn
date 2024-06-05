2d73fc06e8eca84bd1ccffbc1b49078039ab7587
prudhvirapuru <prudhvirapuru@gmail.com>
2024-06-05 18:31:43 +0530

    add Autenticatin for the Tutor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_04_121338) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_05_115526) do
   create_table "students", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,6 +22,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_121338) do
     t.string "name"
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
+  end
+
+  create_table "tutors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_tutors_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_tutors_on_reset_password_token", unique: true
   end
 
 end
