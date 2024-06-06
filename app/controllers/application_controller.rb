b4ff6a3dff2963cd8ee32d46723082ac8dde5dfa
prudhvirapuru <prudhvirapuru@gmail.com>
2024-06-06 16:20:51 +0530

    Add fields for the Tutor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,10 @@ class ApplicationController < ActionController::Base
   
     protected
   
-    def configure_permitted_parameters
-      if resource_class == Student
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-      end 
-    end
     
     def configure_permitted_parameters
-      if resource_class == Tutor
         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone,:education_qualification, :profession, :work_experience ])
         devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone,:education_qualification, :profession, :work_experience])
-      end
     end
 end
   
