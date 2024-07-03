25b4941ded7e7fd75c773b90b4745ae7885d138e
prudhvirapuru <prudhvirapuru@gmail.com>
2024-07-03 14:46:06 +0530

    changes for the tutor-index with user login-name

---
+++ test/system/courses_test.rb
@@ -0,0 +1,53 @@
+require "application_system_test_case"
+
+class CoursesTest < ApplicationSystemTestCase
+  setup do
+    @course = courses(:one)
+  end
+
+  test "visiting the index" do
+    visit courses_url
+    assert_selector "h1", text: "Courses"
+  end
+
+  test "should create course" do
+    visit courses_url
+    click_on "New course"
+
+    fill_in "Duration", with: @course.duration
+    check "Free" if @course.free
+    fill_in "Price", with: @course.price
+    fill_in "Table of contents", with: @course.table_of_contents
+    fill_in "Technologies", with: @course.technologies
+    fill_in "Title", with: @course.title
+    fill_in "Tutor", with: @course.tutor_id
+    click_on "Create Course"
+
+    assert_text "Course was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Course" do
+    visit course_url(@course)
+    click_on "Edit this course", match: :first
+
+    fill_in "Duration", with: @course.duration
+    check "Free" if @course.free
+    fill_in "Price", with: @course.price
+    fill_in "Table of contents", with: @course.table_of_contents
+    fill_in "Technologies", with: @course.technologies
+    fill_in "Title", with: @course.title
+    fill_in "Tutor", with: @course.tutor_id
+    click_on "Update Course"
+
+    assert_text "Course was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Course" do
+    visit course_url(@course)
+    click_on "Destroy this course", match: :first
+
+    assert_text "Course was successfully destroyed"
+  end
+end
