45c8c3c29ed956073dab04d05f06322857f6eff6
prudhvirapuru <prudhvirapuru@gmail.com>
2024-07-06 16:20:25 +0530

    create Editor for the Project

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,39 +1,38 @@
 class CoursesController < ApplicationController
-    before_action :set_tutor
-  
-    def index
-      @courses = @tutor.courses
-    end
-  
-    def show
-      @course = @tutor.courses.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      flash[:alert] = "Course not found."
-      redirect_to tutor_courses_path(@tutor)
-    end
-  
-    def new
-      @course = @tutor.courses.build
-    end
-  
-    def create
-      @course = @tutor.courses.build(course_params)
-  
-      if @course.save
-        redirect_to tutor_courses_path(@tutor), notice: 'Course was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    def set_tutor
-      @tutor = Tutor.find(params[:tutor_id])
-    end
-  
-    def course_params
-      params.require(:course).permit(:title, :technologies, :duration, :table_of_contents, :course_type, :price, :free)
+  before_action :set_tutor
+
+  def index
+    @courses = @tutor.courses
+  end
+
+  def show
+    @course = @tutor.courses.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "Course not found."
+    redirect_to tutor_courses_path(@tutor)
+  end
+
+  def new
+    @course = @tutor.courses.build
+  end
+
+  def create
+    @course = @tutor.courses.build(course_params)
+
+    if @course.save
+      redirect_to tutor_courses_path(@tutor), notice: 'Course was successfully created.'
+    else
+      render :new
     end
   end
-  
+
+  private
+
+  def set_tutor
+    @tutor = Tutor.find(params[:tutor_id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :technologies, :duration, :table_of_contents, :course_type, :price, :free)
+  end
+end
